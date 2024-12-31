f62d7fb2b8c2031f4fda3508a344ae8dc62834b7
TELLYHUBCLOUD <147921265+TELLYHUBCLOUD@users.noreply.github.com>
2024-12-31 19:12:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -16,14 +16,14 @@ def is_enabled(value, default):
 
 ADMIN = int(getenv("ADMIN", ""))
 SILICON_PIC = os.environ.get("SILICON_PIC", "https://telegra.ph/file/21a8e96b45cd6ac4d3da6.jpg")
-API_ID = int(getenv("API_ID", ""))
-API_HASH = str(getenv("API_HASH", ""))
-BOT_TOKEN = str(getenv("BOT_TOKEN", ""))
-FORCE_SUB = os.environ.get("FORCE_SUB", "") 
-MONGO_DB = str(getenv("MONGO_DB", "mongodb+srv://replacewithyourmongodb:replacewithyourmongodb@cluster0.zi78j51.mongodb.net/?retryWrites=true&w=majority",))
+API_ID = int(getenv("API_ID", "23241238"))
+API_HASH = str(getenv("API_HASH", "e6ff6e3068dbea75500865ac49c3608f"))
+BOT_TOKEN = str(getenv("BOT_TOKEN", "7165570655:AAFScTnBANvreZYuyR8TX0QPtToTspp0Qek"))
+FORCE_SUB = os.environ.get("FORCE_SUB", "-1002009012734") 
+MONGO_DB = str(getenv("MONGO_DB", "mongodb+srv://seyov22189:seyov22189@cluster0.rttekkf.mongodb.net/?retryWrites=true&w=majority",))
 DEF_CAP = str(
     getenv(
         "DEF_CAP",
-        "<b>File Name:- `{file_name}`\n\n{file_size}</b>",
+        "<b>File Name:- `File Name: {file_name}`\n\nSize: {file_size}</b>",
     )
 )
